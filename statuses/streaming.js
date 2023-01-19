6d19198c53fce4ab2c0af5bafcd2450c5fc48ae8
GamerKing417 <76030852+GamerKing417@users.noreply.github.com>
2023-01-20 04:08:05 +0530

    Update streaming.js

--- statuses/streaming.js
+++ statuses/streaming.js
@@ -1,11 +1,11 @@
 const
   rpcGenerator = require("discordrpcgenerator"),
 
-  // Add your client id in the index.js file
-  IMAGE_NAME = "The name of the image",
-  LARGE_TEXT = "Large text on the status",
-  SMALL_TEXT = "Small text on the status",
-  LINK = "https://twitch.tv/SealedSaucer";
+  // 1060989610433466429
+  IMAGE_NAME = "logo",
+  LARGE_TEXT = "Master Smp",
+  SMALL_TEXT = "Bot",
+  LINK = "https://twitter.com/GamerKi55404375?t=1WSkZ_aJaWjtbm-At64LMA&s=09";
  
 module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAGE_NAME)
   .then(image => client.user.setPresence(
